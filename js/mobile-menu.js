895f21994dfea4306a1eb0e35bc3ec3364e1a897
RafallPawlak <104594858+RafallPawlak@users.noreply.github.com>
2022-07-05 22:20:21 +0200

    Final

---
+++ js/mobile-menu.js
@@ -0,0 +1,22 @@
+
+function lockScroll() {
+            document.body.classList.toggle('lock-scroll');
+      }
+
+(() => {
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
+    const bodyRef = document.querySelector("[data-body]");
+
+    menuBtnRef.addEventListener("click", () => {
+        const expanded =
+            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+        
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+        mobileMenuRef.classList.toggle("is-open");
+        bodyRef.classList.toggle("is-scrolling-disabled");
+
+        });
+})();
